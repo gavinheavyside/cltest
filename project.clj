c85fac233c0aca48e99b83045c3497cf78356866
Gavin Heavyside <gavin.heavyside@journeydynamics.com>
2010-05-04 10:06:26 +0100

    update project description and version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject cltest "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject cltest "0.1.0-SNAPSHOT"
+  :description "Minimal Cascalog example for Hadoop cluster"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [cascalog "1.0.0-SNAPSHOT"]]
