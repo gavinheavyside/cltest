501500b3459272bee0f0591e8e98922d00b357b2
Gavin Heavyside <gavin@heavyside.co.uk>
2010-05-06 20:05:52 +0100

    add hadoop jar as dependency

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Minimal Cascalog example for Hadoop cluster"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [cascalog "1.0.0-SNAPSHOT"]]
+                 [cascalog "1.0.0-SNAPSHOT"]
+                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]]
   :main cltest.core
   )
